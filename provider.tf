40522f422a19397c3d2f807e3b645fb60f143d8d
Amin Fakhri <n01506010@humber.ca>
2023-03-20 10:08:48 -0400

    thrid update

--- provider.tf
+++ provider.tf
@@ -1,11 +1,4 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
-    }
-  }
-}
+
 provider "aws" {
   region  = "us-west-2"
   shared_config_files      = ["/Users/n01506010/.aws/config"]
