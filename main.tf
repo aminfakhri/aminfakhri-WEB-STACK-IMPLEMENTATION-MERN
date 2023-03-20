f46a1ed87ba56aad5dfa63683e4e34f3cf39e552
Amin Fakhri <n01506010@humber.ca>
2023-03-20 11:49:54 -0400

    update docs

--- main.tf
+++ main.tf
@@ -1,10 +1,14 @@
 
 resource "aws_instance" "proj3" {
-  ami           = "ami-0df24e148fdb9f1d8"
+  ami           = "ami-0735c191cf914754d"
   instance_type = "t2.nano"
   key_name = "pro2"
 
   tags = {
     Name = "WEB STACK DEVELOPMENT MERN"
   }
+  
+  user_data = file("script.sh")
+
 }
+
